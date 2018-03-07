78c830ec4c2670e8e8005f0744f63580dae6c939
shuang <shuang@192.168.241.122>
2018-03-07 18:49:27 +0800

    注册Eureka Server

--- microservice/microservice-consumer-movie/src/main/java/com/itmuch/cloud/Application.java
+++ microservice/microservice-consumer-movie/src/main/java/com/itmuch/cloud/Application.java
@@ -2,14 +2,16 @@ package com.itmuch.cloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class Application {
-	
+
 	@Bean
-	public RestTemplate restTemplate(){
+	public RestTemplate restTemplate() {
 		return new RestTemplate();
 	}
 
