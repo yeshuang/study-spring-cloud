1df3bc4430432599865b67252ba356321452bbbc
shuang <shuang@192.168.239.63>
2018-03-26 09:46:18 +0800

    Ribbon 的学习

--- microservice/microservice-consumer-ribbon-properties/src/main/java/com/itmuch/cloud/ConsumerRibbonPropertiesApplication.java
+++ microservice/microservice-consumer-ribbon-properties/src/main/java/com/itmuch/cloud/ConsumerRibbonPropertiesApplication.java
@@ -4,19 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.FilterType;
 import org.springframework.web.client.RestTemplate;
-
-import com.itmuch.config.ExcludeFromComponentScan;
 
 @SpringBootApplication
 @EnableEurekaClient
-@RibbonClient(name = "provider-user", configuration = CustomizingRibbonClientConfiguration.class)
-@ComponentScan(excludeFilters = { @ComponentScan.Filter(type = FilterType.ANNOTATION, value = ExcludeFromComponentScan.class) })
-public class ConsumerRibbonApplication {
+public class ConsumerRibbonPropertiesApplication {
 
 	@Bean
 	@LoadBalanced
@@ -25,6 +18,6 @@ public class ConsumerRibbonApplication {
 	}
 
 	public static void main(String[] args) {
-		SpringApplication.run(ConsumerRibbonApplication.class, args);
+		SpringApplication.run(ConsumerRibbonPropertiesApplication.class, args);
 	}
 }
